ee790b46a11b50c90f7b75a6a47b9a47b4f783e9
Nihal Yadav <nihalyadav8524@gmail.com>
2024-10-07 21:10:27 +0530

    Made the Onboarding screen appear only once.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kineticqr/views/onboarding.dart';
+import 'package:kineticqr/widgets/Bottomnavbar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
+  final bool isNewUser = prefs.getBool('newUser') ?? true;
   final bool isDarkMode = prefs.getBool('darkMode') ?? false;
-  runApp(MyApp(isDarkMode: isDarkMode));
+  runApp(MyApp(isDarkMode: isDarkMode, isNewUser: isNewUser,));
 }
 
 class MyApp extends StatelessWidget {
   final bool isDarkMode;
+  final bool isNewUser;
 
-  const MyApp({Key? key, required this.isDarkMode}) : super(key: key);
+  const MyApp({Key? key, required this.isDarkMode, required this.isNewUser}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +33,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const Onboarding(),
+      child: isNewUser ? const Onboarding() : BottomAppBarPage(),
     );
   }
 }
